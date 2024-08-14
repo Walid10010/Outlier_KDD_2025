8f760e3d3cd2ce63abeb91acf2c616f23a9fd7d1
Walid10010 <107435785+Walid10010@users.noreply.github.com>
2024-08-14 21:59:53 +0200

    Update

--- ARDEH.py
+++ ARDEH.py
@@ -93,7 +93,6 @@ class ADERH():
 
     def  _nCircle(self, X, cnnIndex, n, distMatrix, center_index):
 
-        # return X, distMatrix
         X = np.append(X, X[cnnIndex], axis=0)
         radius_matrix = np.repeat(distMatrix.reshape(-1,1)/2, 2, axis=1).reshape(-1, 1)
 
@@ -121,7 +120,6 @@ class ADERH():
             [self.n_estimators, factor2 * self.max_samples_, n_features])
         self._centroids_radius2 = np.empty(
             [self.n_estimators, factor2 * self.max_samples_])
-       # print(self._center.shape)
         random_state = check_random_state(self.random_state)
         self._seeds = random_state.randint(MAX_INT, size=self.n_estimators)
 
@@ -137,18 +135,15 @@ class ADERH():
             radom_neigh_idx = rnd.choice(
                 self.max_samples_, self.max_samples_, replace=False)
             random_neigh_idx = radom_neigh_idx
-            # Ensure no sample is paired with itself
 
             for ii in range(len(radom_neigh_idx)):
                 if random_neigh_idx[ii] == ii:
-                    # Find an index to swap with that is not i
-                    swap_idx = (ii + 1) % len(radom_neigh_idx)  # Simple method: just take the next index
+                    swap_idx = (ii + 1) % len(radom_neigh_idx)
                     random_neigh_idx[ii], random_neigh_idx[swap_idx] = random_neigh_idx[swap_idx], random_neigh_idx[ii]
             radom_neigh_idx = random_neigh_idx
-            # print(radom_nei gh_idx)
             random_neigh_dist =  np.sum ((((self._center[i] - X[radom_neigh_idx]) ** 2)), axis=1)
 
-            self._centroids_radius[i] = random_neigh_dist #np.average(random_neigh_dist, axis=0)
+            self._centroids_radius[i] = random_neigh_dist
 
             cnn_index = radom_neigh_idx
             self._center2[i], self._centroids_radius2[i] =  self._nCircle(self._center[i], cnn_index, factor, random_neigh_dist, center_index)
@@ -167,44 +162,6 @@ class ADERH():
 
         X = check_array(X, accept_sparse=False)
         aderh_score = np.ones([self.n_estimators, X.shape[0]])
-        # import matplotlib.pyplot   as plt
-		#
-        # plt.figure(figsize=(10, 10))
-        # #plt.hist(self._centroids_radius2.flatten()/np.max(self._centroids_radius2.flatten()), bins=30, density=True, alpha=0.6, color='b', label='Histogram')
-        # # Normalize radii
-        # radii1 = self._centroids_radius2.flatten()
-        # radii1_normalized = (radii1 - np.min(radii1)) / (np.max(radii1) - np.min(radii1)) + 0.05
-		#
-        # # Calculate the histogram and the corresponding bin edges
-        # # Calculate the histogram with density=True to normalize
-        # counts, bin_edges = np.histogram(radii1_normalized, bins=30, density=True)
-		#
-        # # Calculate the width of each bin
-        # bin_widths = np.diff(bin_edges)
-		#
-        # counts_scaled = counts / np.max(counts)
-		#
-        # # Plotting the scaled density distribution
-        # plt.bar(bin_edges[:-1], counts_scaled, width=np.diff(bin_edges), edgecolor='black', alpha=0.5)
-        # plt.xlabel("Radius", fontsize= 40)
-        # plt.ylabel("Density", fontsize= 40)
-        # #plt.yticks([0,0.25, 0.5, 0.75, 0.1])
-		#
-        # # Optional: Set specific ticks for the x-axis (if needed)
-        # plt.xticks([0, 0.25, 0.5, 0.75, 1])
-        # plt.grid(False)
-        # plt.tick_params(axis='both', which='major', labelsize=36)
-        # plt.tick_params(axis='both', which='minor', labelsize=20)
-        # plt.tight_layout()
-        # plt.xlim(0.01, 1.01)
-        # plt.ylim(0.01, 1.01)
-		#
-        # total_area = np.sum(counts * bin_widths)
-        # print(f"Total area under the curve: {total_area}")
-        # plt.savefig('fig/{}_{}.jpg'.format(self.data_name, self.index))
-		#
-        # #plt.legend()
-        # plt.show()
 
         self._center, self._centroids_radius = self._center2, self._centroids_radius2
 
@@ -224,10 +181,7 @@ class ADERH():
             dicc = dict(zip(unique, counts))
 
             v = cover_radius[x_covered[0], cnn_x]/self._centroids_radius2[i][cnn_x]
-            #print(v.max(), v.min())
 
-           # v = adjusted_sigmoid(v)
-            # v = abfall(v)
 
             den = self._loop_translate(cnn_x, dicc)
             den /=self._centroids_radius2[i][cnn_x]
